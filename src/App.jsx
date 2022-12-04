2e6ceac8ecc95fadfdc38ce4e553c4e520a60ae7
Davner Firmino Cavalcante <davner23@gmail.com>
2022-12-04 12:46:38 -0400

    💽Install: Instalação de arquivos - 04/12/2022

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-center font-bold text-3xl capitalize hover:text-blue-600'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
